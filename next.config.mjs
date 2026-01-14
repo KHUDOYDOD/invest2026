37621216204e70d121dd2ab15c3388c1cf0060e8
KHUDOYDOD <97233862+KHUDOYDOD@users.noreply.github.com>
2026-01-14 23:08:16 +0500

    Fix __dirname for ES modules

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -13,7 +19,7 @@ const nextConfig = {
     // Явно указываем пути для резолва модулей
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': __dirname,
+      '@': path.resolve(__dirname),
     }
     
     if (!isServer) {
